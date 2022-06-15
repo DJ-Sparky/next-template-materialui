80191c9f523b9fa769e3170e823d2baef33befa9
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-06-15 18:15:43 +0800

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1>NextJS Template</h1>
+      <Button variant="contained">With Material-UI</Button>
+    </>
+  );
+};
+
+export default Home;
